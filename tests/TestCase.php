6bd6b23ef83e3ba6c2e05a9a9ca4625af0c17d37
Faisal Budiono <ucul.project@gmail.com>
2023-02-14 11:37:42 +0700

    Add faker instance in base TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,10 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use WithFaker;
 }
