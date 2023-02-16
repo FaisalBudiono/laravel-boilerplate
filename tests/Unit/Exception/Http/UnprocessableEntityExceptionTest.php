4d651fd601b0d2b31b7c12598bbe445fb238ae87
Faisal Budiono <ucul.project@gmail.com>
2023-02-17 03:22:21 +0700

    Add unprocessable entity HTTP exception

---
+++ tests/Unit/Exception/Http/UnprocessableEntityExceptionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit\Exception\Http;
+
+use App\Exceptions\Http\UnprocessableEntityException;
+use Symfony\Component\HttpFoundation\Response;
+
+class UnprocessableEntityExceptionTest extends HttpExceptionBaseTestCase
+{
+    protected function getHttpStatusCode(): int
+    {
+        return Response::HTTP_UNPROCESSABLE_ENTITY;
+    }
+
+    protected function makeHttpException(): string
+    {
+        return UnprocessableEntityException::class;
+    }
+}
