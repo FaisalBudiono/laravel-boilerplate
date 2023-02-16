f4245da4c46e984c8b8dc47b265d4adbbe9b5be4
Faisal Budiono <ucul.project@gmail.com>
2023-02-17 02:30:19 +0700

    Refactor conflict exception test suite

---
+++ tests/Unit/Exception/Http/HttpExceptionBaseTestCase.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Unit\Exception\Http;
+
+use App\Core\Formatter\ExceptionMessage\ExceptionMessage;
+use Exception;
+use Illuminate\Support\Collection;
+use Mockery\MockInterface;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+abstract class HttpExceptionBaseTestCase extends TestCase
+{
+    protected string $mockedMessage;
+    protected Collection $mockedJsonResponse;
+    protected ExceptionMessage $mockExceptionMessage;
+
+    abstract protected function getHttpStatusCode(): int;
+    abstract protected function makeHttpException(): string;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockedMessage = 'some error message';
+        $this->mockedJsonResponse = collect([
+            'message' => 'something',
+        ]);
+
+        $this->mockExceptionMessage = $this->mock(ExceptionMessage::class, function (
+            MockInterface $mock
+        ) {
+            $mock->shouldReceive('getMessage')->andReturn($this->mockedMessage);
+            $mock->shouldReceive('getJsonResponse')->andReturn($this->mockedJsonResponse);
+        });
+    }
+
+    #[Test]
+    public function getCode_should_return_right_json_content()
+    {
+        // Act
+        $exception = new ($this->makeHttpException())($this->mockExceptionMessage);
+        $result = $exception->getCode();
+
+
+        // Assert
+        $this->assertEquals($this->getHttpStatusCode(), $result);
+    }
+
+    #[Test]
+    public function getMessage_should_return_right_json_content()
+    {
+        // Act
+        $exception = new ($this->makeHttpException())($this->mockExceptionMessage);
+        $result = $exception->getMessage();
+
+
+        // Assert
+        $this->assertEquals($this->mockedMessage, $result);
+    }
+
+    #[Test]
+    #[DataProviderExternal(DataProvider::class, 'previousExceptions')]
+    public function getPrevious_should_return_right_json_content(?Exception $mockPreviousException)
+    {
+        // Act
+        $exception = new ($this->makeHttpException())(
+            $this->mockExceptionMessage,
+            $mockPreviousException
+        );
+        $result = $exception->getPrevious();
+
+
+        // Assert
+        $this->assertEquals($mockPreviousException, $result);
+    }
+
+    #[Test]
+    public function render_should_return_right_json_content()
+    {
+        // Act
+        $exception = new ($this->makeHttpException())($this->mockExceptionMessage);
+        $result = $exception->render('');
+
+
+        // Assert
+        $this->assertEqualsCanonicalizing(
+            $this->mockedJsonResponse->toArray(),
+            json_decode($result->content(), true)
+        );
+    }
+}
