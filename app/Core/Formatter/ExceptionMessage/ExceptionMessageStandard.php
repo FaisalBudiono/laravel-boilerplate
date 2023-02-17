1910fe7f4580132010b640fafcab98d3bb9cc6d4
Faisal Budiono <ucul.project@gmail.com>
2023-02-17 11:01:37 +0700

    Add standard exception message

---
+++ app/Core/Formatter/ExceptionMessage/ExceptionMessageStandard.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Core\Formatter\ExceptionMessage;
+
+use Illuminate\Support\Collection;
+
+class ExceptionMessageStandard implements ExceptionMessage
+{
+    public function __construct(
+        protected string $errorMessage,
+        protected string $errorCode,
+        protected ?array $meta = [],
+    ) {
+    }
+
+    public function getJsonResponse(): Collection
+    {
+        return collect([
+            'message' => $this->errorMessage,
+            'errorCode' => $this->errorCode,
+            'meta' => $this->meta,
+        ]);
+    }
+
+    public function getMessage(): string
+    {
+        return $this->errorMessage;
+    }
+}
---
+++ tests/Unit/Core/Formatter/ExceptionMessage/ExceptionMessageStandardTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Core\Formatter\ExceptionMessage;
+
+use Illuminate\Support\Collection;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class ExceptionMessageStandardTest extends TestCase
+{
+    #[Test]
+    public function should_implement_right_interface()
+    {
+        // Arrange
+        $exceptionMessage = new ExceptionMessageStandard('some-error', 'some-const');
+
+
+        // Assert
+        $this->assertInstanceOf(ExceptionMessage::class, $exceptionMessage);
+    }
+
+    #[Test]
+    public function getJsonResponse_should_return_formatted_json_response_when_constructor_is_completely_filled()
+    {
+        // Arrange
+        $message = $this->faker->words(3, true);
+        $errorCode = $this->faker->word();
+        $meta = Collection::times(4)->mapWithKeys(fn () => [
+            $this->faker->word => $this->faker->words(),
+        ])->toArray();
+
+
+        // Act
+        $exceptionMessage = new ExceptionMessageStandard(
+            $message,
+            $errorCode,
+            $meta,
+        );
+        $result = $exceptionMessage->getJsonResponse();
+
+
+        // Assert
+        $this->assertEqualsCanonicalizing(collect([
+            'message' => $message,
+            'errorCode' => $errorCode,
+            'meta' => $meta,
+        ]), $result);
+    }
+
+    #[Test]
+    public function getJsonResponse_should_return_formatted_json_response_when_created_without_meta()
+    {
+        // Arrange
+        $message = $this->faker->words(3, true);
+        $errorCode = $this->faker->word();
+
+
+        // Act
+        $exceptionMessage = new ExceptionMessageStandard(
+            $message,
+            $errorCode,
+        );
+        $result = $exceptionMessage->getJsonResponse();
+
+
+        // Assert
+        $this->assertEqualsCanonicalizing(collect([
+            'message' => $message,
+            'errorCode' => $errorCode,
+            'meta' => [],
+        ]), $result);
+    }
+
+    #[Test]
+    public function getMessage_should_return_message()
+    {
+        // Arrange
+        $message = $this->faker->words(3, true);
+
+
+        // Act
+        $exceptionMessage = new ExceptionMessageStandard($message, 'some-const');
+        $result = $exceptionMessage->getMessage();
+
+
+        // Assert
+        $this->assertSame($message, $result);
+    }
+}
