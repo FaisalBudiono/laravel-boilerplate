57419e4ceed1733a831502a6fd3c9dbdccaab6a6
Faisal Budiono <ucul.project@gmail.com>
2023-04-20 08:09:34 +0700

    Add getAll() in UserCore

--- app/Core/User/UserCore.php
+++ app/Core/User/UserCore.php
@@ -3,11 +3,15 @@
 namespace App\Core\User;
 
 use App\Core\Formatter\ExceptionMessage\ExceptionMessageStandard;
+use App\Core\Query\OrderDirection;
+use App\Core\User\Query\UserOrderBy;
 use App\Exceptions\Core\User\UserEmailDuplicatedException;
 use App\Models\User\Enum\UserExceptionCode;
 use App\Models\User\User;
 use App\Port\Core\User\CreateUserPort;
+use App\Port\Core\User\GetAllUserPort;
 use App\Port\Core\User\GetUserPort;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -46,5 +50,17 @@ class UserCore implements UserCoreContract
     public function get(GetUserPort $request): User
     {
         return $request->getUserModel();
+    }
+
+    public function getAll(GetAllUserPort $request): LengthAwarePaginator
+    {
+        $page = $request->getPage() ?? 1;
+        $perPage = $request->getPerPage() ?? 30;
+        $orderDirection = $request->getOrderDirection() ?? OrderDirection::DESCENDING;
+        $orderBy = $request->getOrderBy() ?? UserOrderBy::CREATED_AT;
+
+        return User::query()
+            ->orderBy($orderBy->value, $orderDirection->value)
+            ->paginate($perPage, ['*'], 'page', $page);
     }
 }
--- app/Core/User/UserCoreContract.php
+++ app/Core/User/UserCoreContract.php
@@ -4,10 +4,13 @@ namespace App\Core\User;
 
 use App\Models\User\User;
 use App\Port\Core\User\CreateUserPort;
+use App\Port\Core\User\GetAllUserPort;
 use App\Port\Core\User\GetUserPort;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface UserCoreContract
 {
     public function create(CreateUserPort $request): User;
     public function get(GetUserPort $request): User;
+    public function getAll(GetAllUserPort $request): LengthAwarePaginator;
 }
