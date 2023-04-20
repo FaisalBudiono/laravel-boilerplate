2605c58ab35094c320245b75aa75766c3fce4363
Faisal Budiono <ucul.project@gmail.com>
2023-04-20 09:17:37 +0700

    Register CoreServiceProvider in app config

--- config/app.php
+++ config/app.php
@@ -193,6 +193,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\CoreServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
