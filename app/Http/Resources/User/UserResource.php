e8c88f8fd2903d5ce78e31c183adb0b32f9ee25c
Faisal Budiono <ucul.project@gmail.com>
2023-04-24 03:21:41 +0700

    Add User model resource

---
+++ app/Http/Resources/User/UserResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use App\Core\Date\DatetimeFormat;
+use App\Models\User\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        /** @var User */
+        $resource = $this->resource;
+
+        return [
+            'id' => $resource->id,
+            'name' => $resource->name,
+            'email' => $resource->email,
+            'created_at' => optional($resource->created_at)->format(DatetimeFormat::ISO_WITH_MILLIS->value),
+            'updated_at' => optional($resource->updated_at)->format(DatetimeFormat::ISO_WITH_MILLIS->value),
+        ];
+    }
+}
