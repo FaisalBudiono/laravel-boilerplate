dda9005880da8ef77f6f342b496f2aedb51d8c44
Faisal Budiono <ucul.project@gmail.com>
2023-04-24 07:03:28 +0700

    Remove web route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,9 +31,6 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
         });
     }
 
