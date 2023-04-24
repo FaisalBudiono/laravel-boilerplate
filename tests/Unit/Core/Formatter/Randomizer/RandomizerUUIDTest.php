0071541a883c03981e7764b305a7161e58008666
Faisal Budiono <ucul.project@gmail.com>
2023-04-24 07:53:47 +0700

    Add UUID Randomizer

---
+++ tests/Unit/Core/Formatter/Randomizer/RandomizerUUIDTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\Core\Formatter\Randomizer;
+
+use App\Core\Formatter\Randomizer\Randomizer;
+use App\Core\Formatter\Randomizer\RandomizerUUID;
+use Illuminate\Support\Str;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class RandomizerUUIDTest extends TestCase
+{
+    #[Test]
+    public function should_implement_right_interface()
+    {
+        // Assert
+        $this->assertInstanceOf(Randomizer::class, $this->makeService());
+    }
+
+    #[Test]
+    public function getRandomizeString_should_randomized_uuid()
+    {
+        // Arrange
+        $service = $this->makeService();
+
+
+        // Act
+        $result = $service->getRandomizeString();
+
+
+        // Assert
+        $this->assertTrue(Str::isUuid($result));
+    }
+
+    protected function makeService(): RandomizerUUID
+    {
+        return new RandomizerUUID;
+    }
+}
