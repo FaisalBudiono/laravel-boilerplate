f6e06702f71562e56ce598e4f3687f17f00a303b
Faisal Budiono <ucul.project@gmail.com>
2023-07-13 17:21:41 +0700

    Add token pair for authentication

---
+++ tests/Unit/Core/Auth/JWT/ValueObject/TokenPairTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit\Core\Auth\JWT\ValueObject;
+
+use App\Core\Auth\JWT\ValueObject\TokenPair;
+use Illuminate\Contracts\Support\Arrayable;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class TokenPairTest extends TestCase
+{
+    #[Test]
+    public function should_implement_right_interface()
+    {
+        // Arrange
+        $valueObject = new TokenPair(
+            $this->faker->sentence(),
+            $this->faker->sentence(),
+        );
+
+
+        // Assert
+        $this->assertInstanceOf(Arrayable::class, $valueObject);
+    }
+
+    #[Test]
+    public function toArray_should_map_data_correctly()
+    {
+        // Arrange
+        $valueObject = new TokenPair(
+            $this->faker->sentence(),
+            $this->faker->sentence(),
+        );
+
+
+        // Act
+        $result = $valueObject->toArray();
+
+
+        // Assert
+        $expectedResult = [
+            'type' => 'Bearer',
+            'accessToken' => $valueObject->accessToken,
+            'refreshToken' => $valueObject->refreshToken,
+        ];
+        $this->assertEquals($expectedResult, $result);
+    }
+}
