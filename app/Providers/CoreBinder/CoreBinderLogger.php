27c5421c7a9e7932aa43cfe614ceb3cecb025ecd
Faisal Budiono <ucul.project@gmail.com>
2023-07-16 23:25:29 +0700

    Register logger message factory in service provider

--- app/Providers/CoreBinder/CoreBinderLogger.php
+++ app/Providers/CoreBinder/CoreBinderLogger.php
@@ -2,14 +2,23 @@
 
 namespace App\Providers\CoreBinder;
 
+use App\Core\Logger\Message\LoggerMessageFactory;
+use App\Core\Logger\Message\LoggerMessageFactoryContract;
 use App\Core\Logger\MessageFormatter\LoggerMessageFormatterFactory;
 use App\Core\Logger\MessageFormatter\LoggerMessageFormatterFactoryContract;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\Request;
 
 class CoreBinderLogger implements CoreBinder
 {
     public function bootCore(Application $app): void
     {
+        $app->bind(LoggerMessageFactoryContract::class, function (Application $app) {
+            return new LoggerMessageFactory(
+                $app->make(Request::class),
+            );
+        });
+
         $app->bind(LoggerMessageFormatterFactoryContract::class, function (Application $app) {
             return new LoggerMessageFormatterFactory;
         });
