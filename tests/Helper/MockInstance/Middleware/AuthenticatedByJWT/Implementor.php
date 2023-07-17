e38b320eca27f6da9ed1dddc3ecb04a23b7e5967
Faisal Budiono <ucul.project@gmail.com>
2023-07-17 15:27:50 +0700

    Add assertion helper for JWT authentication middleware

---
+++ tests/Helper/MockInstance/Middleware/AuthenticatedByJWT/Implementor.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Helper\MockInstance\Middleware\AuthenticatedByJWT;
+
+use App\Core\Auth\JWT\JWTGuardContract;
+use App\Http\Middleware\AuthenticatedByJWT;
+use App\Models\User\User;
+use Exception;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class Implementor extends TestCase
+{
+    protected AuthenticatedByJWT $jwtMiddleware;
+    protected JWTGuardContract $jwtGuard;
+
+    protected MockInterface $mockInterfaceJWTMiddleware;
+    protected MockInterface $mockInterfaceJWTGuard;
+
+    public function __construct(
+        protected TestCase $test,
+    ) {
+        $this->jwtMiddleware = $this->test->mock(
+            AuthenticatedByJWT::class,
+            function (MockInterface $mock) {
+                $this->mockInterfaceJWTMiddleware = $mock;
+            }
+        );
+        $this->jwtGuard = $this->test->mock(
+            JWTGuardContract::class,
+            function (MockInterface $mock) {
+                $this->mockInterfaceJWTGuard = $mock;
+            }
+        );
+    }
+
+    public static function make(TestCase $test): self
+    {
+        return new self($test);
+    }
+
+    public function bindInstance(): void
+    {
+        $this->test->instance(AuthenticatedByJWT::class, $this->jwtMiddleware);
+        $this->test->instance(JWTGuardContract::class, $this->jwtGuard);
+    }
+
+    public function mockLogin(User $user): self
+    {
+        $this->mockInterfaceJWTMiddleware
+            ->shouldReceive('handle')
+            ->once()
+            ->andReturnUsing(function ($argRequest, $argNext) {
+                return $argNext($argRequest);
+            });
+
+        $this->mockInterfaceJWTGuard->shouldReceive('user')
+            ->withNoArgs()
+            ->andReturn($user);
+
+        return $this;
+    }
+
+    public function mockGuestAndThrow(Exception $e): self
+    {
+        $this->mockInterfaceJWTMiddleware->shouldReceive('handle')->once()->andThrow($e);
+
+        $this->mockInterfaceJWTGuard->shouldReceive('user')
+            ->withNoArgs()
+            ->andReturnNull();
+
+        return $this;
+    }
+}
---
+++ tests/Helper/MockInstance/Middleware/MockerAuthenticatedByJWT.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Helper\MockInstance\Middleware;
+
+use App\Models\User\User;
+use Exception;
+use Tests\Helper\MockInstance\Middleware\AuthenticatedByJWT\Implementor;
+use Tests\TestCase;
+
+class MockerAuthenticatedByJWT
+{
+    protected Implementor $implementor;
+
+    public function __construct(
+        TestCase $test,
+    ) {
+        $this->implementor = Implementor::make($test);
+    }
+
+    public static function make(TestCase $test): self
+    {
+        return new self($test);
+    }
+
+    public function bindInstance(): void
+    {
+        $this->implementor->bindInstance();
+    }
+
+    public function mockLogin(User $user): self
+    {
+        $this->implementor->mockLogin($user);
+        return $this;
+    }
+
+    public function mockGuestAndThrow(Exception $e): self
+    {
+        $this->implementor->mockGuestAndThrow($e);
+        return $this;
+    }
+}
