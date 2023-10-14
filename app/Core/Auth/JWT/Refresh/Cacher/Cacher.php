b494a6d734062f714eca571dba60e62db047e24a
Faisal Budiono <ucul.project@gmail.com>
2023-10-15 03:50:12 +0700

    Implement grace period in refresh token manager

--- app/Core/Auth/JWT/Refresh/Cacher/Cacher.php
+++ app/Core/Auth/JWT/Refresh/Cacher/Cacher.php
@@ -8,8 +8,5 @@ interface Cacher
 {
     public function deleteAllGenerations(string $tokenID): void;
     public function find(string $tokenID): RefreshTokenClaims;
-    public function invalidate(string $tokenID): void;
-    public function isUnused(string $tokenID): bool;
     public function save(RefreshTokenClaims $refreshTokenClaims): void;
-    public function setChildID(string $parentID, string $childID): void;
 }
--- tests/Unit/Core/Auth/JWT/Refresh/Cacher/CacherLaravel/CacherLaravel_Save_Test.php
+++ tests/Unit/Core/Auth/JWT/Refresh/Cacher/CacherLaravel/CacherLaravel_Save_Test.php
@@ -6,30 +6,24 @@ use App\Core\Auth\JWT\Refresh\ValueObject\RefreshTokenClaims;
 use App\Core\Auth\JWT\Refresh\ValueObject\RefreshTokenClaimsUser;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 
 class CacherLaravel_Save_Test extends CacherLaravelBaseTestCase
 {
     #[Test]
-    public function should_cache_all_info()
+    #[DataProvider('tokenDataProvider')]
+    public function should_cache_all_info(RefreshTokenClaims $mockedToken): void
     {
-        // Arrange
-        $mockedToken = new RefreshTokenClaims(
-            $this->faker->uuid(),
-            new RefreshTokenClaimsUser($this->faker->uuid(), $this->faker->email),
-            Carbon::parse($this->faker->dateTimeBetween('1 years', '10 years')),
-        );
-
-
         // Assert
         Cache::shouldReceive('putMany')
             ->with(
                 [
                     "{$this->getPrefixName()}:{$mockedToken->id}:user:id" => $mockedToken->user->id,
                     "{$this->getPrefixName()}:{$mockedToken->id}:user:email" => $mockedToken->user->userEmail,
-                    "{$this->getPrefixName()}:{$mockedToken->id}:child:id" => $mockedToken->childID,
-                    "{$this->getPrefixName()}:{$mockedToken->id}:is-unused" => 0,
                     "{$this->getPrefixName()}:{$mockedToken->id}:expired-at" => $mockedToken->expiredAt->unix(),
+                    "{$this->getPrefixName()}:{$mockedToken->id}:child:id" => $mockedToken->childID,
+                    "{$this->getPrefixName()}:{$mockedToken->id}:used-at" => $mockedToken->usedAt?->unix(),
                 ],
                 $mockedToken->expiredAt,
             )->once();
@@ -37,5 +31,32 @@ class CacherLaravel_Save_Test extends CacherLaravelBaseTestCase
 
         // Act
         $this->makeService()->save($mockedToken);
+    }
+
+    public static function tokenDataProvider(): array
+    {
+        $faker = self::makeFaker();
+
+        return [
+            'complete refresh token' => [
+                new RefreshTokenClaims(
+                    $faker->uuid(),
+                    new RefreshTokenClaimsUser($faker->uuid(), $faker->email),
+                    Carbon::parse($faker->dateTime()),
+                    $faker->uuid(),
+                    Carbon::parse($faker->dateTime()),
+                ),
+            ],
+
+            'without nullable field' => [
+                new RefreshTokenClaims(
+                    $faker->uuid(),
+                    new RefreshTokenClaimsUser($faker->uuid(), $faker->email),
+                    Carbon::parse($faker->dateTime()),
+                    null,
+                    null,
+                ),
+            ],
+        ];
     }
 }
