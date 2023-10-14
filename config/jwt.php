672b57ae71822386562b0b3b312b6a0ef7d86de2
Faisal Budiono <ucul.project@gmail.com>
2023-10-15 03:44:44 +0700

    Add ENV for refresh token grace period in seconds

--- config/jwt.php
+++ config/jwt.php
@@ -13,6 +13,7 @@ return [
     'refresh' => [
         'prefix' => env('JWT_REFRESH_PREFIX', 'jwt-refresh'),
         'ttl' => env('JWT_REFRESH_TTL_IN_SECONDS', 86400),
+        'grace-period' => env('JWT_REFRESH_GRACE_PERIOD_SECONDS', 5),
     ],
     'ttl' => env('JWT_TTL_IN_SECONDS', 5),
 ];
