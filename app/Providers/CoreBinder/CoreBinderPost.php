008d1ab78c38c5621926238b8c166c61f2f86daf
Faisal Budiono <ucul.project@gmail.com>
2023-11-16 11:50:11 +0700

    feat(policy): register PostCore in service provider

---
+++ app/Providers/CoreBinder/CoreBinderPost.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Providers\CoreBinder;
+
+use App\Core\Post\PostCore;
+use App\Core\Post\PostCoreContract;
+use Illuminate\Contracts\Foundation\Application;
+
+class CoreBinderPost implements CoreBinder
+{
+    public function bootCore(Application $app): void
+    {
+        $app->bind(PostCoreContract::class, function (Application $app) {
+            return new PostCore;
+        });
+    }
+}
---
+++ tests/Unit/Providers/CoreBinder/CoreBinderTestCasePost.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Providers\CoreBinder;
+
+use App\Core\Post\PostCore;
+use App\Core\Post\PostCoreContract;
+
+class CoreBinderTestCasePost extends CoreBinderTestCaseAbstract
+{
+    protected function abstractWithImplementationList(): array
+    {
+        return [
+            PostCoreContract::class => [
+                PostCore::class,
+            ],
+        ];
+    }
+}
