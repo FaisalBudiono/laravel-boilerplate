20c5b5f9b442e88a4cde636f5b4f2dbe7c9b85be
Faisal Budiono <ucul.project@gmail.com>
2023-12-25 23:15:38 +0700

    feat(logger): register log builder in service provider

--- app/Providers/CoreBinder/CoreBinderLogger.php
+++ app/Providers/CoreBinder/CoreBinderLogger.php
@@ -6,6 +6,10 @@ namespace App\Providers\CoreBinder;
 
 use App\Core\Logger\Message\LoggerMessageFactory;
 use App\Core\Logger\Message\LoggerMessageFactoryContract;
+use App\Core\Logger\Message\LogMessageBuilder;
+use App\Core\Logger\Message\LogMessageBuilderContract;
+use App\Core\Logger\Message\LogMessageDirector;
+use App\Core\Logger\Message\LogMessageDirectorContract;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
 
@@ -16,6 +20,18 @@ class CoreBinderLogger implements CoreBinder
         $app->bind(
             LoggerMessageFactoryContract::class,
             fn (Application $app) => new LoggerMessageFactory(
+                $app->make(Request::class),
+            )
+        );
+
+        $app->bind(
+            LogMessageBuilderContract::class,
+            fn (Application $app) => new LogMessageBuilder()
+        );
+
+        $app->bind(
+            LogMessageDirectorContract::class,
+            fn (Application $app) => new LogMessageDirector(
                 $app->make(Request::class),
             )
         );
--- tests/Unit/Providers/CoreBinder/CoreBinderTestCaseLogger.php
+++ tests/Unit/Providers/CoreBinder/CoreBinderTestCaseLogger.php
@@ -6,6 +6,10 @@ namespace Tests\Unit\Providers\CoreBinder;
 
 use App\Core\Logger\Message\LoggerMessageFactory;
 use App\Core\Logger\Message\LoggerMessageFactoryContract;
+use App\Core\Logger\Message\LogMessageBuilder;
+use App\Core\Logger\Message\LogMessageBuilderContract;
+use App\Core\Logger\Message\LogMessageDirector;
+use App\Core\Logger\Message\LogMessageDirectorContract;
 use Illuminate\Http\Request;
 use Tests\Unit\Providers\CoreBinder\Dependencies\DependencyFactoryRaw;
 
@@ -16,6 +20,15 @@ class CoreBinderTestCaseLogger extends CoreBinderTestCaseAbstract
         return [
             LoggerMessageFactoryContract::class => [
                 LoggerMessageFactory::class,
+                [
+                    new DependencyFactoryRaw(new Request()),
+                ],
+            ],
+            LogMessageBuilderContract::class => [
+                LogMessageBuilder::class,
+            ],
+            LogMessageDirectorContract::class => [
+                LogMessageDirector::class,
                 [
                     new DependencyFactoryRaw(new Request()),
                 ],
