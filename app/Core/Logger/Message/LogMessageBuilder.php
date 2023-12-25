83b785c17f2d3890290cc87f3df3adc7cdc8820e
Faisal Budiono <ucul.project@gmail.com>
2023-12-25 21:00:44 +0700

    feat(logging): add LogMessageBuilder

---
+++ app/Core/Logger/Message/LogMessageBuilder.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Logger\Message;
+
+use App\Core\Logger\Message\ValueObject\LogMessage;
+
+class LogMessageBuilder implements LogMessageBuilderContract
+{
+    protected ?string $endpoint;
+    protected ?string $requestID;
+    protected ?ProcessingStatus $processingStatus;
+    protected ?string $message;
+    protected ?array $meta;
+
+    public function endpoint(string $endpoint): self
+    {
+        $this->endpoint = $endpoint;
+        return $this;
+    }
+
+    public function requestID(string $requestID): self
+    {
+        $this->requestID = $requestID;
+        return $this;
+    }
+
+    public function processingStatus(ProcessingStatus $processingStatus): self
+    {
+        $this->processingStatus = $processingStatus;
+        return $this;
+    }
+
+    public function message(string $message): self
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    public function meta(array $meta): self
+    {
+        $this->meta = $meta;
+        return $this;
+    }
+
+    public function build(): LogMessage
+    {
+        return new LogMessage(
+            $this->endpoint ?? '',
+            $this->requestID ?? '',
+            $this->processingStatus ?? ProcessingStatus::BEGIN,
+            $this->message ?? '',
+            $this->meta ?? [],
+        );
+    }
+}
---
+++ tests/Unit/Core/Logger/Message/LogMessageBuilderTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Core\Logger\Message;
+
+use App\Core\Logger\Message\LogMessageBuilder;
+use App\Core\Logger\Message\LogMessageBuilderContract;
+use App\Core\Logger\Message\ProcessingStatus;
+use App\Core\Logger\Message\ValueObject\LogMessage;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class LogMessageBuilderTest extends TestCase
+{
+    #[Test]
+    public function should_implement_right_interface(): void
+    {
+        // Assert
+        $this->assertInstanceOf(LogMessageBuilderContract::class, $this->makeService());
+    }
+
+    #[Test]
+    public function should_build_log_message_properly(): void
+    {
+        // Arrange
+        $expectedResult =  new LogMessage(
+            $endpoint = $this->faker->sentence(),
+            $requestID = $this->faker->sentence(),
+            $processingStatus = $this->faker->randomElement(ProcessingStatus::cases()),
+            $message = $this->faker->sentence(),
+            $meta = $this->faker->sentences(),
+        );
+
+
+        // Assert
+        $result = $this->makeService()
+            ->endpoint($endpoint)
+            ->requestID($requestID)
+            ->processingStatus($processingStatus)
+            ->message($message)
+            ->meta($meta)
+            ->build();
+
+
+        // Assert
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    #[Test]
+    public function should_build_log_message_properly_with_default_value(): void
+    {
+        // Arrange
+        $expectedResult =  new LogMessage(
+            '',
+            '',
+            ProcessingStatus::BEGIN,
+            '',
+            [],
+        );
+
+
+        // Assert
+        $result = $this->makeService()->build();
+
+
+        // Assert
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    protected function makeService(): LogMessageBuilder
+    {
+        return new LogMessageBuilder();
+    }
+}
