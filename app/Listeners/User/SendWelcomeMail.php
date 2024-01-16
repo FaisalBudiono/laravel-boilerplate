e0d63bf0cfd92568b796dac05c1c26e9fb6023da
Faisal Budiono <ucul.project@gmail.com>
2024-01-16 17:20:28 +0700

    feat: add logging in listener

--- app/Listeners/User/SendWelcomeMail.php
+++ app/Listeners/User/SendWelcomeMail.php
@@ -2,10 +2,15 @@
 
 namespace App\Listeners\User;
 
+use App\Core\Formatter\Randomizer\Randomizer;
+use App\Core\Logger\Message\Enum\LogEndpoint;
+use App\Core\Logger\Message\LogMessageBuilderContract;
+use App\Core\Logger\Message\LogMessageDirectorContract;
 use App\Events\User\UserCreated;
 use App\Mail\User\WelcomeMail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class SendWelcomeMail implements ShouldQueue
@@ -13,8 +18,11 @@ class SendWelcomeMail implements ShouldQueue
     /**
      * Create the event listener.
      */
-    public function __construct()
-    {
+    public function __construct(
+        protected Randomizer $randomizer,
+        protected LogMessageBuilderContract $logMessageBuilder,
+        protected LogMessageDirectorContract $logMessageDirector,
+    ) {
         //
     }
 
@@ -23,6 +31,40 @@ class SendWelcomeMail implements ShouldQueue
      */
     public function handle(UserCreated $event): void
     {
-        Mail::send(new WelcomeMail($event->user));
+        try {
+            $requestID = $this->randomizer->getRandomizeString();
+
+            Log::info(
+                $this->logMessageDirector->buildBegin(
+                    clone $this->logMessageBuilder,
+                )->requestID($requestID)
+                    ->endpoint(LogEndpoint::QUEUE->value)
+                    ->message(self::class)
+                    ->meta([
+                        'user' => $event->user->toArray(),
+                    ])->build()
+            );
+
+            Mail::send(new WelcomeMail($event->user));
+
+            Log::info(
+                $this->logMessageDirector->buildSuccess(
+                    clone $this->logMessageBuilder,
+                )->requestID($requestID)
+                    ->endpoint(LogEndpoint::QUEUE->value)
+                    ->message(self::class)
+                    ->build()
+            );
+        } catch (\Throwable $e) {
+            Log::error(
+                $this->logMessageDirector->buildSuccess(
+                    clone $this->logMessageBuilder,
+                )->requestID($requestID)
+                    ->endpoint(LogEndpoint::QUEUE->value)
+                    ->message(self::class)
+                    ->build()
+            );
+            throw $e;
+        }
     }
 }
